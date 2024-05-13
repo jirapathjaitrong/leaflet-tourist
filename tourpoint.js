2546e9859183a0611e8050b8e1bf591d53a863bc
jirapathjaitrong <158020278+jirapathjaitrong@users.noreply.github.com>
2024-05-13 16:47:49 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,157 @@
+var tourpoint = 
+{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Kho Yai National Park",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            101.49696550393259,
+            14.332197218072508
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Grand Palace ",
+          "type": "cutural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.49161272883708,
+            13.75114324116123
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Koh Sirey beach",
+          "type": "natural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            98.44120052513074,
+            7.909781133811279
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "๋Jomtien beach",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            100.87186078378664,
+            12.892949532283183
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Wat Saket",
+          "type": "cutural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.50708195767163,
+            13.753952581562558
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Mae Ramphueng Beach",
+          "type": "ืnatural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            101.37256825985315,
+            12.61765173894375
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "teay ngam beach",
+          "type": "natural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.8611761018505,
+            12.66998374068902
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Wat Pho",
+          "type": "cutural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            100.49329187122834,
+            13.746594217183613
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Wat Phai Rong Wua",
+          "type": "cutural",
+          "level": 3
+        },
+        "geometry": {
+          "coordinates": [
+            100.16037972883657,
+            14.160183862741178
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Wat Suthat",
+          "type": "curtural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.50112091618337,
+            13.751126040283523
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
+    
